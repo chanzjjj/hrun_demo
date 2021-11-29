548350ea6864d8656f1fdcaf73f2450510800f34
chenzj <harry.czj@mobvoyage.com>
2021-11-29 17:45:28 +0800

    更新代码

--- debugtalk.py
+++ debugtalk.py
@@ -1,5 +1,12 @@
 
-import time
+def get_account(num):
+    '''生成多组测试数据'''
+    accounts = []
+    for i in range(1, num+1):
+        accounts.append(
+            {"user": "test%s" %i, "psw": "123456"}
+        )
+    return accounts
 
-def sleep(n_secs):
-    time.sleep(n_secs)
+if __name__ == '__main__':
+    print(get_account(10))
